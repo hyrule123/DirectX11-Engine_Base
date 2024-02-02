6a02856c04594d7e209eee5cbb56d24bb23cddf8
HWNOTE <ekdrn1@naver.com>
2024-02-03 01:08:16 +0900

    enum, enum class에 대한 처리 방법 추가

--- Engine/Util/Serialize/JsonSerializer.cpp
+++ Engine/Util/Serialize/JsonSerializer.cpp
@@ -19,5 +19,6 @@ namespace ehw
 	{
 		return eResult::Fail_NotImplemented;
 	}
+
 }
 
--- Engine/Util/Serialize/JsonSerializer.h
+++ Engine/Util/Serialize/JsonSerializer.h
@@ -9,6 +9,8 @@
 
 namespace ehw
 {
+	
+
 	class JsonSerializer : public Serializer
 	{
 	public:
@@ -19,29 +21,29 @@ namespace ehw
 		virtual eResult LoadFile(std::filesystem::path const& _fullPath) override;
 
 		Json::Value& operator[] (const std::string_view _strKey) { return m_jVal[_strKey]; }
-		const Json::Value& operator[] (const std::string_view _strKey) const { return m_jVal[_strKey]; }
+		const Json::Value& operator[] (const std::string_view _strKey) const { return m_jVal[_strKey]; };
+		bool isMember(const std::string_view _strKey) const { return m_jVal.isMember(_strKey); }
 
 	private:
 		Json::Value m_jVal;
 	};
 
-
+	//템플릿 특수화를 하고자 하는 타입은 여기 추가 후 특수화해주면 됨.
+	template <typename T>
+	concept JsonTypeSpecialization =
+		std::is_enum_v<T>;
 
 	template <typename T>
 	concept JsonDefaultTypes =
 		!type_traits_Ex::PointerTypes<T> &&
-		requires (T t)
-	{
-		{ Json::Value(t) };
-	};
+		!JsonTypeSpecialization<T> &&
+		requires (T t) { { Json::Value(t) }; };
+
 	template <typename T>
 	concept NotJsonDefaultTypes =
 		!type_traits_Ex::PointerTypes<T> &&
-		!requires (T t)
-	{
-		{ Json::Value(t) };
-	};
-
+		!JsonTypeSpecialization<T> &&
+		!requires (T t) { { Json::Value(t) }; };
 
 	template <JsonDefaultTypes T>
 	inline void operator <<(Json::Value& _jVal, const T& _data)
@@ -58,31 +60,25 @@ namespace ehw
 	//컨테이너는 StringConverter에서 함수를 별도로 만들어줄것.
 	//또한 혹시 별도의 처리를 해야 하는 데이터 타입이 있을 경우 이 함수에 대해 템플릿 특수화를 해줄것.
 	template <NotJsonDefaultTypes T>
-	requires (!std::is_enum_v<T>)
 	inline void operator <<(Json::Value& _jVal, const T& _data)
 	{
 		_jVal = StringConverter::Base64Encode(_data);
 	}
 
-	template <NotJsonDefaultTypes T>
-	requires std::is_enum_v<T>
-	inline void operator <<(Json::Value& _jVal, T _data)
-	{
-		//https://stackoverflow.com/questions/8357240/how-to-automatically-convert-strongly-typed-enum-into-int
-		_jVal = static_cast<std::underlying_type_t<T>>(_data);
-	}
 
-	inline void operator <<(Json::Value& _jVal, const char* _data)
+	inline void operator <<(Json::Value& _jVal, const char* _c_str)
 	{
-		if (nullptr != _data)
+		if (nullptr != _c_str)
 		{
-			_jVal = Json::StaticString(_data);
+			_jVal = Json::StaticString(_c_str);
 		}
 	}
 
-
-
-
+	template <typename T> requires std::is_enum_v<T>
+	inline void operator <<(Json::Value& _jVal, T _data)
+	{
+		_jVal = static_cast<std::underlying_type_t<T>>(_data);
+	}
 
 	template <JsonDefaultTypes T>
 	inline void operator >>(const Json::Value& _jVal, T& _data)
@@ -91,17 +87,15 @@ namespace ehw
 	}
 
 	template <NotJsonDefaultTypes T>
-		requires (!std::is_enum_v<T>)
 	inline void operator >>(const Json::Value& _jVal, T& _data)
 	{
 		_data = StringConverter::Base64Decode<T>(_jVal.asString());
 	}
 
-	template <NotJsonDefaultTypes T>
-		requires std::is_enum_v<T>
-	inline void operator <<(const Json::Value& _jVal, T& _data)
+
+	template <typename T> requires std::is_enum_v<T>
+	inline void operator >>(const Json::Value& _jVal, T& _data)
 	{
-		//https://stackoverflow.com/questions/8357240/how-to-automatically-convert-strongly-typed-enum-into-int
 		_data = static_cast<T>(_jVal.as<std::underlying_type_t<T>>());
 	}
 }
