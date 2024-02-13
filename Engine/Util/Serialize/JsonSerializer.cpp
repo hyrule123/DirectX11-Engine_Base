596e78619356a745bf90bad7653da9bde6b75070
HWNOTE <ekdrn1@naver.com>
2024-02-13 23:11:04 +0900

    (컴파일불가)JsonSerializer에 float3, float4, MATRIX 처리 코드 추가

--- Engine/Util/Serialize/JsonSerializer.cpp
+++ Engine/Util/Serialize/JsonSerializer.cpp
@@ -1,7 +1,10 @@
 #include "JsonSerializer.h"
 
+#include "../../CommonType.h"
+
 namespace ehw
 {
+
 	JsonSerializer::JsonSerializer()
 		: m_jVal(Json::objectValue)
 	{
@@ -20,5 +23,77 @@ namespace ehw
 		return eResult::Fail_NotImplemented;
 	}
 
+
+	void operator<<(Json::Value& _jVal, const char* _c_str)
+	{
+		if (nullptr != _c_str)
+		{
+			_jVal = Json::StaticString(_c_str);
+		}
+	}
+	void operator>>(const Json::Value& _jVal, const char* _c_str)
+	{
+		const auto& pair = g_cstrArchive.insert(_jVal.asString());
+		_c_str = pair.first->c_str();
+	}
+
+	void operator<<(Json::Value& _jVal, const float3& _f3)
+	{
+		_jVal.clear();
+		_jVal.append(_f3.x);
+		_jVal.append(_f3.y);
+		_jVal.append(_f3.z);
+	}
+
+	void operator>>(const Json::Value& _jVal, float3& _f3)
+	{
+		_f3.x = _jVal[0].asFloat();
+		_f3.y = _jVal[1].asFloat();
+		_f3.z = _jVal[2].asFloat();
+	}
+
+	void operator<<(Json::Value& _jVal, const float4& _f4)
+	{
+		_jVal.clear();
+		_jVal.append(_f4.x);
+		_jVal.append(_f4.y);
+		_jVal.append(_f4.z);
+		_jVal.append(_f4.w);
+	}
+
+	void operator>>(const Json::Value& _jVal, float4& _f4)
+	{
+		_f4.x = _jVal[0].asFloat();
+		_f4.y = _jVal[1].asFloat();
+		_f4.z = _jVal[2].asFloat();
+		_f4.w = _jVal[3].asFloat();
+	}
+
+	void operator<<(Json::Value& _jVal, const MATRIX& _m)
+	{
+		_jVal.clear();
+		_jVal.resize(4);
+		for (int i = 0; i < 4; ++i)
+		{
+			Json::Value& col = _jVal[i];
+			col.resize(4);
+			for (int j = 0; j < 4; ++j)
+			{
+				col[j] = _m.m[i][j];
+			}
+		}
+	}
+	void operator>>(const Json::Value& _jVal, MATRIX& _m)
+	{
+		for (int i = 0; i < 4; ++i)
+		{
+			for (int j = 0; j < 4; ++j)
+			{
+				_m.m[i][j] = _jVal[i][j].asFloat();
+			}
+		}
+	}
 }
+
+
 
--- Engine/Util/Serialize/JsonSerializer.h
+++ Engine/Util/Serialize/JsonSerializer.h
@@ -2,6 +2,8 @@
 #include "Serializer.h"
 
 #include "../../CommonGlobalVar.h"
+#include "../../CommonType.h"
+
 #include "../StringConverter.h"
 #include "json.h"
 
@@ -11,7 +13,8 @@
 
 namespace ehw
 {
-	class JsonSerializer : public Serializer
+	class JsonSerializer 
+		: public Serializer
 	{
 	public:
 		JsonSerializer();
@@ -28,11 +31,17 @@ namespace ehw
 		Json::Value m_jVal;
 	};
 
+	//템플릿을 통한 1차 타입 분류
+	//Json 기본 생성자에서 지원하는 타입의 경우 그대로 추가하고,
+	//미지원 타입인 경우 JsonSerializer에서 오버로드된 함수를 호출함.
 	//템플릿 특수화를 하고자 하는 타입은 여기 추가 후 특수화해주면 됨.
 	template <typename T>
-	concept JsonTypeSpecialization = 
+	concept JsonTypeSpecialization =
 		type_traits_Ex::PointerTypes<T> ||
-		type_traits_Ex::is_enum_class_v<T>;
+		type_traits_Ex::is_enum_class_v<T> ||
+		std::is_same_v<float3, T> ||
+		std::is_same_v<float4, T> ||
+		std::is_same_v<MATRIX, T>;
 
 	template <typename T>
 	concept JsonDefaultTypes =
@@ -40,7 +49,7 @@ namespace ehw
 		requires (T t) { { Json::Value(t) }; };
 
 	template <typename T>
-	concept NotJsonDefaultTypes =
+	concept JsonBase64Types =
 		!JsonTypeSpecialization<T> &&
 		!requires (T t) { { Json::Value(t) }; };
 
@@ -56,25 +65,15 @@ namespace ehw
 		_jVal = std::move(_data);
 	}
 
-	//타입을 가리지 않고 포인터 제외 데이터를 Base64로 전환하므로 주의할것.
+	//특수화하지 않았을 경우 타입을 가리지 않고 포인터 제외 데이터를 Base64로 전환하므로 주의할것.
 	//컨테이너는 StringConverter에서 함수를 별도로 만들어줄것.
 	//또한 혹시 별도의 처리를 해야 하는 데이터 타입이 있을 경우 이 함수에 대해 템플릿 특수화를 해줄것.
-	template <NotJsonDefaultTypes T>
+	template <JsonBase64Types T>
 	inline void operator <<(Json::Value& _jVal, const T& _data)
 	{
 		_jVal = StringConverter::Base64Encode(_data);
 	}
 	
-	//c_str
-	template <JsonTypeSpecialization T> requires type_traits_Ex::PointerTypes<T>
-	inline void operator <<(Json::Value& _jVal, T _c_str)
-	{
-		static_assert(std::is_same_v(T, char), "c_str 문자열만 사용 가능");
-		if (nullptr != _c_str)
-		{
-			_jVal = Json::StaticString(_c_str);
-		}
-	}
 
 	//enum class
 	template <JsonTypeSpecialization T> requires type_traits_Ex::is_enum_class_v<T>
@@ -89,7 +88,7 @@ namespace ehw
 		_data = _jVal.as<T>();
 	}
 
-	template <NotJsonDefaultTypes T>
+	template <JsonBase64Types T>
 	inline void operator >>(const Json::Value& _jVal, T& _data)
 	{
 		_data = StringConverter::Base64Decode<T>(_jVal.asString());
@@ -102,11 +101,23 @@ namespace ehw
 		_data = static_cast<T>(_jVal.as<std::underlying_type_t<T>>());
 	}
 
-	template <JsonTypeSpecialization T> requires type_traits_Ex::PointerTypes<T>
-	inline void operator >> (const Json::Value& _jVal, T _c_str)
-	{
-		static_assert(std::is_same_v<T, const char*>, "c_str 포인터(const char*)만 지원함.");
-		const auto& pair = g_cstrArchive.insert(_jVal.asString());
-		_c_str = pair.first->c_str();
-	}
+
+#pragma region 특수화
+	//c_str
+	void operator << (Json::Value& _jVal, const char* _c_str);
+	void operator >> (const Json::Value& _jVal, const char* _c_str);
+
+	//float3
+	void operator << (Json::Value& _jVal, const float3& _f3);
+	void operator >> (const Json::Value& _jVal, float3& _f3);
+
+	//float4
+	void operator << (Json::Value& _jVal, const float4& _f4);
+	void operator >> (const Json::Value& _jVal, float4& _f4);
+
+	//MATRIX
+	void operator << (Json::Value& _jVal, const MATRIX& _m);
+	void operator >> (const Json::Value& _jVal, MATRIX& _m);
+
+#pragma endregion //특수화
 }
